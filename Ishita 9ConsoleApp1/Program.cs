8c1f2c9fe62ad18b90faabbe8aab7797c38df9a3
A <A@ANUJ>
2023-09-30 21:33:16 -0400

    Add project files.

---
+++ Ishita 9ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ishita_9ConsoleApp1
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n Recursion : Find the factorial of a given number: ");
+            Console.WriteLine(".......................................................");
+            Console.WriteLine("Input any possitive number :");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            long fact = FactorialCalcu(n1);
+            Console.WriteLine("The Factorial of {0} is :{1}", n1, fact);
+            Console.ReadLine();
+        }
+        private static long FactorialCalcu(int n1);
+        
+         if (n1 == 0)
+            {
+            return 1;
+            }
+            return n1 * FactorialCalcu(n1-1);
+      }
+    }
+
